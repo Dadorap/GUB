e5d83f41817dfe04f5c93b18bdac0dafea3edb06
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-05 16:54:50 +0100

    updated services in program.cs

--- GUB/Program.cs
+++ GUB/Program.cs
@@ -1,3 +1,4 @@
+using DataAccessLayer.Models;
 using GUB.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -12,12 +13,13 @@ namespace GUB
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            builder.Services.AddDbContext<BankAppDataContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<BankAppDataContext>();
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
