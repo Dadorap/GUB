a23ca766fd5a8099c994ba315b6f289726239a1e
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-05 16:53:27 +0100

    did some configuration inside my BankAppDataContext class

--- DataAccessLayer/Models/BankAppDataContext.cs
+++ DataAccessLayer/Models/BankAppDataContext.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Models;
 
-public partial class BankAppDataContext : DbContext
+public partial class BankAppDataContext : IdentityDbContext
 {
     public BankAppDataContext()
     {
@@ -29,14 +30,14 @@ public partial class BankAppDataContext : DbContext
 
     public virtual DbSet<Transaction> Transactions { get; set; }
 
-    public virtual DbSet<User> Users { get; set; }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=localhost;Database=BankAppData;Trusted_Connection=True;TrustServerCertificate=true;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
 
         modelBuilder.Entity<Account>(entity =>
