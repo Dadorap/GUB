58f0da2eb221f32fd07577f827d387bf5230fbc9
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-10 17:55:05 +0100

    created new page Register and change title

---
+++ GUB/Pages/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GUB.Pages
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
