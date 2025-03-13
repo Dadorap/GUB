fa1982d312576ee8fac5c3936888d764532c3743
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-13 13:17:50 +0100

    added current time to my cards

--- GUB/Pages/Index.cshtml.cs
+++ GUB/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 using DataAccessLayer.DTOs;
 using GUB.API;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 
 namespace GUB.Pages;
 
@@ -15,9 +16,13 @@ public class IndexModel : PageModel
 
 
     public List<ZenQuotesDTO> ZenQuotes { get; set; } = new List<ZenQuotesDTO>();
+    public string DateOnly { get; set; }
 
     public async Task OnGet()
     {
-        ZenQuotes = await _zenQuotesService.GetQuotes();
+        ZenQuotes = await _zenQuotesService.GetQuotes(); DateTime now = DateTime.Now;
+        DateOnly = now.ToString("dddd, MMMM d 'at' HH:mm", CultureInfo.InvariantCulture);
+
+
     }
 }
