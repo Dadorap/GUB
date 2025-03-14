27fe259dbc1f86e2c2a80e4441311018fc06417d
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-14 10:37:56 +0100

    added about and contact us page

---
+++ GUB/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GUB.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
