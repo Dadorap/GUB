6942da7088022fb0c50188b000ce4d3c25b5f889
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-14 10:21:50 +0100

    started with footer

--- GUB/Pages/Index.cshtml.cs
+++ GUB/Pages/Index.cshtml.cs
@@ -2,7 +2,6 @@ using DataAccessLayer.DTOs;
 using GUB.API;
 using GUB.ViewModel.LandingPage;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Services.BusinessLogic.LandingPage;
 using System.Globalization;
 
 namespace GUB.Pages;
@@ -12,14 +11,15 @@ public class IndexModel : PageModel
     private readonly ZenQuotesService _zenQuotesService;
     private readonly ICountryCardData _countryCardData;
 
-    public IndexModel(ZenQuotesService zenQuotesService, ICountryCardData countryCardData)
+    public IndexModel(ZenQuotesService zenQuotesService,
+                      ICountryCardData countryCardData)
     {
         _zenQuotesService = zenQuotesService;
         _countryCardData = countryCardData;
     }
 
 
-    public List<ZenQuotesDTO> ZenQuotes { get; set; } = new List<ZenQuotesDTO>();
+    public List<ZenQuotesDTO> ZenQuotes { get; set; } = new();
     public List<LandingPageCardViewModel> SweCard { get; set; } = new();
     public List<LandingPageCardViewModel> FinCard { get; set; } = new();
     public List<LandingPageCardViewModel> NorCard { get; set; } = new();
@@ -28,7 +28,7 @@ public class IndexModel : PageModel
 
     public async Task OnGet()
     {
-        ZenQuotes = await _zenQuotesService.GetQuotes(); 
+        ZenQuotes = await _zenQuotesService.GetQuotes();
         DateTime now = DateTime.Now;
         DateOnly = now.ToString("dddd, MMMM d 'at' HH:mm", CultureInfo.InvariantCulture);
 
--- GUB/ViewModel/LandingPage/CountryCardData.cs
+++ GUB/ViewModel/LandingPage/CountryCardData.cs
@@ -1,76 +1,74 @@
-﻿using GUB.API;
-using Services.BusinessLogic.LandingPage;
+﻿using Services.BusinessLogic.LandingPage;
 using System.Globalization;
 
-namespace GUB.ViewModel.LandingPage
+namespace GUB.ViewModel.LandingPage;
+
+public class CountryCardData : ICountryCardData
 {
-    public class CountryCardData : ICountryCardData
+    private readonly ICardsInfoService _cardsInfoService;
+
+    public CountryCardData(ICardsInfoService cardsInfoService)
     {
-        private readonly ICardsInfoService _cardsInfoService;
 
-        public CountryCardData(ICardsInfoService cardsInfoService)
+        _cardsInfoService = cardsInfoService;
+    }
+    public List<LandingPageCardViewModel> GetCountryData(string country)
+    {
+        if (country == "Sweden")
         {
-
-            _cardsInfoService = cardsInfoService;
+            return _cardsInfoService.GetCountryList("Sweden").Select(c => new LandingPageCardViewModel
+            {
+                Country = c.Country,
+                Accounts = c.Accounts.ToString("#,##0", CultureInfo.InvariantCulture)
+                .Replace(",", " "),
+                Balance = c.Balance.ToString("#,##0.00", CultureInfo.InvariantCulture)
+                .Replace(",", " ")
+                .Replace(".", ","),
+                Transactions = c.Transactions.ToString("#,##0", CultureInfo.InvariantCulture)
+                .Replace(",", " ")
+            }).ToList();
         }
-        public List<LandingPageCardViewModel> GetCountryData(string country)
+        else if (country == "Finland")
         {
-            if (country == "Sweden")
+            return _cardsInfoService.GetCountryList("Finland").Select(c => new LandingPageCardViewModel
             {
-                return _cardsInfoService.GetCountryList("Sweden").Select(c => new LandingPageCardViewModel
-                {
-                    Country = c.Country,
-                    Accounts = c.Accounts.ToString("#,##0", CultureInfo.InvariantCulture)
-                    .Replace(",", " "),
-                    Balance = c.Balance.ToString("#,##0.00", CultureInfo.InvariantCulture)
-                    .Replace(",", " ")
-                    .Replace(".", ","),
-                    Transactions = c.Transactions.ToString("#,##0", CultureInfo.InvariantCulture)
-                    .Replace(",", " ")
-                }).ToList();
-            }
-            else if (country == "Finland")
+                Country = c.Country,
+                Accounts = c.Accounts.ToString("#,##0", CultureInfo.InvariantCulture)
+                .Replace(",", " "),
+                Balance = c.Balance.ToString("#,##0.00", CultureInfo.InvariantCulture)
+                .Replace(",", " ")
+                .Replace(".", ","),
+                Transactions = c.Transactions.ToString("#,##0", CultureInfo.InvariantCulture)
+                .Replace(",", " ")
+            }).ToList();
+        }
+        else if (country == "Denmark")
+        {
+            return _cardsInfoService.GetCountryList("Denmark").Select(c => new LandingPageCardViewModel
             {
-                return _cardsInfoService.GetCountryList("Finland").Select(c => new LandingPageCardViewModel
-                {
-                    Country = c.Country,
-                    Accounts = c.Accounts.ToString("#,##0", CultureInfo.InvariantCulture)
-                    .Replace(",", " "),
-                    Balance = c.Balance.ToString("#,##0.00", CultureInfo.InvariantCulture)
-                    .Replace(",", " ")
-                    .Replace(".", ","),
-                    Transactions = c.Transactions.ToString("#,##0", CultureInfo.InvariantCulture)
-                    .Replace(",", " ")
-                }).ToList();
-            }
-            else if (country == "Denmark")
+                Country = c.Country,
+                Accounts = c.Accounts.ToString("#,##0", CultureInfo.InvariantCulture)
+                .Replace(",", " "),
+                Balance = c.Balance.ToString("#,##0.00", CultureInfo.InvariantCulture)
+                .Replace(",", " ")
+                .Replace(".", ","),
+                Transactions = c.Transactions.ToString("#,##0", CultureInfo.InvariantCulture)
+                .Replace(",", " ")
+            }).ToList();
+        }
+        else
+        {
+            return _cardsInfoService.GetCountryList("Norway").Select(c => new LandingPageCardViewModel
             {
-                return _cardsInfoService.GetCountryList("Denmark").Select(c => new LandingPageCardViewModel
-                {
-                    Country = c.Country,
-                    Accounts = c.Accounts.ToString("#,##0", CultureInfo.InvariantCulture)
-                    .Replace(",", " "),
-                    Balance = c.Balance.ToString("#,##0.00", CultureInfo.InvariantCulture)
-                    .Replace(",", " ")
-                    .Replace(".", ","),
-                    Transactions = c.Transactions.ToString("#,##0", CultureInfo.InvariantCulture)
-                    .Replace(",", " ")
-                }).ToList();
-            }
-            else
-            {
-                return _cardsInfoService.GetCountryList("Norway").Select(c => new LandingPageCardViewModel
-                {
-                    Country = c.Country,
-                    Accounts = c.Accounts.ToString("#,##0", CultureInfo.InvariantCulture)
-                    .Replace(",", " "),
-                    Balance = c.Balance.ToString("#,##0.00", CultureInfo.InvariantCulture)
-                    .Replace(",", " ")
-                    .Replace(".", ","),
-                    Transactions = c.Transactions.ToString("#,##0", CultureInfo.InvariantCulture)
-                    .Replace(",", " ")
-                }).ToList();
-            }
+                Country = c.Country,
+                Accounts = c.Accounts.ToString("#,##0", CultureInfo.InvariantCulture)
+                .Replace(",", " "),
+                Balance = c.Balance.ToString("#,##0.00", CultureInfo.InvariantCulture)
+                .Replace(",", " ")
+                .Replace(".", ","),
+                Transactions = c.Transactions.ToString("#,##0", CultureInfo.InvariantCulture)
+                .Replace(",", " ")
+            }).ToList();
         }
     }
 }
