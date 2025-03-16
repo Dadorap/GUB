ad5280e2164997742b165b55feaf98f6e4f78c57
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-16 14:12:48 +0100

    added new table styling

--- GUB/Pages/Customer.cshtml.cs
+++ GUB/Pages/Customer.cshtml.cs
@@ -1,3 +1,5 @@
+using DataAccessLayer.DTOs;
+using GUB.API;
 using GUB.ViewModel.Customers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,16 +13,22 @@ namespace GUB.Pages
     public class CustomerModel : PageModel
     {
         private readonly ICustomerService _customerService;
+        private readonly ZenQuotesService _zenQuotesService;
 
-        public CustomerModel(ICustomerService customerService)
+
+        public CustomerModel(ICustomerService customerService, ZenQuotesService zenQuotesService)
         {
+            _zenQuotesService = zenQuotesService;
             _customerService = customerService;
         }
 
         public List<CustomerViewModel> Customers { get; set; }
+        public List<ZenQuotesDTO> ZenQuotes { get; set; } = new();
 
-        public void OnGet(string sortColumn, string sortOrder)
+
+        public async Task OnGet(string sortColumn, string sortOrder)
         {
+            ZenQuotes = await _zenQuotesService.GetQuotes();
             Customers = _customerService.GetCustomers(sortColumn, sortOrder)
                 .Select(s => new CustomerViewModel
                 {
