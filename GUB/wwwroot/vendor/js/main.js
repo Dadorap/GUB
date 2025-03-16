de8ebb1326fd8d064922bb02ea8a238ea8ac93f6
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-16 14:51:26 +0100

    fixed issue with logging where clicking on eye span didn't reveal the pw

--- GUB/wwwroot/vendor/js/main.js
+++ GUB/wwwroot/vendor/js/main.js
@@ -134,3 +134,28 @@
   window.addEventListener("load", initSwiper);
 
 })();
+
+
+
+(function ($) {
+    "use strict";
+
+    var fullHeight = function () {
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.js-fullheight').css('height', $(window).height());
+        });
+    };
+    fullHeight();
+
+    $(".toggle-password").click(function () {
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        var input = $($(this).attr("data-toggle")); // Fix here!
+        if (input.attr("type") === "password") {
+            input.attr("type", "text");
+        } else {
+            input.attr("type", "password");
+        }
+    });
+
+})(jQuery);
