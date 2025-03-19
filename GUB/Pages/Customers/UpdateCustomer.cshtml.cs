f24367f658fb46de1efe814022e4e17a17a7f9ae
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-19 21:38:51 +0100

    added edit customer button to my customer details

---
+++ GUB/Pages/Customers/UpdateCustomer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GUB.Pages.Customers
+{
+    public class UpdateCustomerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
