8333cbddfe714d2ff6bc448ecb2c0d750823f6ef
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-24 09:45:54 +0100

    created a new class with method to check country code and country name match

---
+++ Services/BusinessLogic/Customers/CountryValidation.cs
@@ -0,0 +1,27 @@
+﻿namespace Services.BusinessLogic.Customers;
+
+public class CountryValidation
+{
+    private readonly Dictionary<CountryCode, string> CountryNames = new()
+{
+    { CountryCode.SE, "Sweden" },
+    { CountryCode.FI, "Finland" },
+    { CountryCode.DK, "Denmark" },
+    { CountryCode.NO, "Norway" }
+};
+
+    public RespCode ValidateCountryCodeAndName(string codeInput, string countryInput)
+    {
+        if (!Enum.TryParse<CountryCode>(codeInput.Trim().ToUpperInvariant(), out var parsedCode))
+            return RespCode.InvalidCountry;
+
+        if (!CountryNames.TryGetValue(parsedCode, out var expectedCountry))
+            return RespCode.InvalidCountry;
+
+        if (!string.Equals(expectedCountry, countryInput.Trim(), StringComparison.OrdinalIgnoreCase))
+            return RespCode.InvalidCountry;
+
+        return RespCode.OK;
+    }
+
+}
--- Services/BusinessLogic/Transactions/AccountService.cs
+++ Services/BusinessLogic/Transactions/AccountService.cs
@@ -8,7 +8,8 @@ public enum RespCode
     BalanceTooLow,
     IncorrectAmount,
     InvalidMessage,
-    InvalidDate
+    InvalidDate,
+    InvalidCountry
 }
 
 namespace Services.BusinessLogic.Transactions
