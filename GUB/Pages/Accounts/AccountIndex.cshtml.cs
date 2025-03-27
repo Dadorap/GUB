977b5b5aecf3754ff8dfa80b6503f0d8a607396d
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-27 10:40:25 +0100

    added a template table to accounts table

--- GUB/Pages/Accounts/AccountIndex.cshtml.cs
+++ GUB/Pages/Accounts/AccountIndex.cshtml.cs
@@ -1,3 +1,6 @@
+using GUB.API;
+using GUB.ViewModel.Customers;
+using GUB.ViewModel.ZenQuotes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,40 @@ namespace GUB.Pages.Accounts
 {
     public class AccountIndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ZenQuotesService _zenQuotesService;
+
+        public AccountIndexModel(ZenQuotesService zenQuotesService)
         {
+            _zenQuotesService = zenQuotesService;
+        }
+        public List<CustomerViewModel> Customers { get; set; }
+        public List<ZenQuotesViewModel> ZenQuotes { get; set; }
+        public string SortColumn { get; set; }
+        public string SortOrder { get; set; }
+        public int CurrentPage { get; set; }
+        public string Q { get; set; }
+        public int PageCount { get; set; }
+
+        public async Task OnGet(string sortColumn, string sortOrder, int pageNo, string q)
+        {
+            Q = q;
+            SortColumn = sortColumn;
+            SortOrder = sortOrder;
+            if (pageNo == 0)
+                pageNo = 1;
+            CurrentPage = pageNo;
+
+            //var result = _customerService.GetCustomers(SortColumn, SortOrder, CurrentPage, q);
+            //PageCount = result.PageCount;
+
+
+
+            ZenQuotes = (await _zenQuotesService.GetQuotes())
+                        .Select(q => new ZenQuotesViewModel
+                        {
+                            Quote = q.Quote,
+                            Author = q.Author
+                        }).ToList();
         }
     }
 }
