586902303bf777cacf3264aafb23d93bde5374f2
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-28 10:31:57 +0100

    added a template to my account detail page

--- GUB/Pages/Accounts/AccountDetails.cshtml.cs
+++ GUB/Pages/Accounts/AccountDetails.cshtml.cs
@@ -1,12 +1,59 @@
+using GUB.API;
+using GUB.ViewModel.Accounts;
+using GUB.ViewModel.ZenQuotes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.BusinessLogic.AccountManagement;
 
 namespace GUB.Pages.Accounts
 {
     public class AccountDetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ZenQuotesService _zenQuotesService;
+        private readonly IAccountService _accountService;
+
+        public AccountDetailsModel(ZenQuotesService zenQuotesService, IAccountService accountService)
         {
+            _zenQuotesService = zenQuotesService;
+            _accountService = accountService;
+        }
+        public List<AccountsViewModel> Accounts { get; set; }
+        public List<ZenQuotesViewModel> ZenQuotes { get; set; }
+        public string SortColumn { get; set; }
+        public string SortOrder { get; set; }
+        public int CurrentPage { get; set; }
+        public string Q { get; set; }
+        public int PageCount { get; set; }
+
+        public async Task OnGet(string sortColumn, string sortOrder, int pageNo, string q)
+        {
+            Q = q;
+            SortColumn = sortColumn;
+            SortOrder = sortOrder;
+            if (pageNo == 0)
+                pageNo = 1;
+            CurrentPage = pageNo;
+
+            var result = _accountService.GetAccounts(SortColumn, SortOrder, CurrentPage, q);
+            PageCount = result.PageCount;
+
+
+
+            ZenQuotes = (await _zenQuotesService.GetQuotes())
+                        .Select(q => new ZenQuotesViewModel
+                        {
+                            Quote = q.Quote,
+                            Author = q.Author
+                        }).ToList();
+            Accounts = result.Results.Select(x => new AccountsViewModel
+            {
+                AccountId = x.AccountId,
+                CustomerId = x.CustomerId,
+                CustomerFirstName = x.CustomerFirstName,
+                CustomerLastName = x.CustomerLastName,
+                Frequency = x.Frequency,
+            }).ToList();
+
         }
     }
 }
