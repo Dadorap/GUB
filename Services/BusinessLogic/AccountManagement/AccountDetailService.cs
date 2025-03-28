abe3b553d660a30cdf77f35d9cc50f8a51f85b91
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-28 10:51:36 +0100

    transaction service now return a list of transaction history biased by accountid

--- Services/BusinessLogic/AccountManagement/AccountDetailService.cs
+++ Services/BusinessLogic/AccountManagement/AccountDetailService.cs
@@ -1,5 +1,38 @@
-﻿namespace Services.BusinessLogic.AccountManagement;
+﻿using DataAccessLayer.DTOs;
+using DataAccessLayer.Models;
 
-public class AccountDetailService
+namespace Services.BusinessLogic.AccountManagement;
+
+public class AccountDetailService : IAccountDetailService
 {
+    private readonly BankAppDataContext _bankAppDataContext;
+
+    public AccountDetailService(BankAppDataContext bankAppDataContext)
+    {
+        _bankAppDataContext = bankAppDataContext;
+    }
+
+    public List<TransactionDTO> GetAccountDetails(int id)
+    {
+       var trans = _bankAppDataContext.Transactions
+            .Where(t => t.AccountId == id)
+            .AsQueryable();
+        if (trans == null) return null;
+
+        var accTrans = trans.Select(s=> new TransactionDTO()
+        {
+            TransactionId = s.TransactionId,
+            AccountId = s.AccountId,
+            Date = s.Date,
+            Type = s.Type,
+            Operation = s.Operation,
+            Amount = s.Amount,
+            Balance = s.Balance,
+            Symbol = s.Symbol,
+            Bank = s.Bank,
+            Account = s.Account,
+        }).ToList();
+
+        return accTrans;
+    }
 }
--- Services/BusinessLogic/AccountManagement/IAccountDetailService.cs
+++ Services/BusinessLogic/AccountManagement/IAccountDetailService.cs
@@ -1,5 +1,8 @@
-﻿namespace Services.BusinessLogic.AccountManagement;
+﻿using DataAccessLayer.DTOs;
+
+namespace Services.BusinessLogic.AccountManagement;
 
 public interface IAccountDetailService
 {
+    List<TransactionDTO> GetAccountDetails(int id);
 }
