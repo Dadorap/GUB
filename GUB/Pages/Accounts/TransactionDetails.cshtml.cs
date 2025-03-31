acc4eaa72f1481f381d9b07edb72c25a0100a024
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-03-31 17:53:27 +0200

    added show more to transaction page

--- GUB/Pages/Accounts/TransactionDetails.cshtml.cs
+++ GUB/Pages/Accounts/TransactionDetails.cshtml.cs
@@ -1,6 +1,8 @@
 using GUB.API;
+using GUB.Infrastructure.Paging;
 using GUB.ViewModel.Accounts;
 using GUB.ViewModel.ZenQuotes;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Services.BusinessLogic.AccountManagement;
 
@@ -16,13 +18,13 @@ namespace GUB.Pages.Accounts
             _zenQuotesService = zenQuotesService;
             _transacitonDetailService = transacitonDetailService;
         }
-        public List<TransactionDetailsViewModel> Transactions { get; set; }
+        //public List<TransactionDetailsViewModel> Transactions { get; set; }
         public List<ZenQuotesViewModel> ZenQuotes { get; set; }
         public string Q { get; set; }
         public int AccountId { get; set; }
         public int CurrentPage { get; set; }
 
-        public async Task OnGet(int id, string q)
+        public  async Task OnGet(int id, string q)
         {
             Q = q;
             ZenQuotes = (await _zenQuotesService.GetQuotes())
@@ -31,23 +33,31 @@ namespace GUB.Pages.Accounts
                             Quote = q.Quote,
                             Author = q.Author
                         }).ToList();
+            AccountId = id;
+        }
 
-            Transactions = _transacitonDetailService.GetTransactionDetails(id).Select(s => new TransactionDetailsViewModel()
-            {
-                TransactionId = s.TransactionId,
-                AccountId = s.AccountId,
-                Date = s.Date,
-                Type = s.Type,
-                Operation = s.Operation,
-                Amount = s.Amount,
-                Balance = s.Balance,
-                Symbol = s.Symbol,
-                Bank = s.Bank,
-                Account = s.Account
+        public IActionResult OnGetShowMore(int id, int pageNo)
+        {
+            var listOfTransa = _transacitonDetailService.GetTransactionDetails(id)
+                .Where(s => s.AccountId == id)
+                .AsQueryable()
+                .GetPaged(pageNo, 10).Results
+                .Select(s => new TransactionDetailsViewModel()
+                {
+                    TransactionId = s.TransactionId,
+                    AccountId = s.AccountId,
+                    Date = s.Date,
+                    Type = s.Type,
+                    Operation = s.Operation,
+                    Amount = s.Amount,
+                    Balance = s.Balance,
+                    Symbol = s.Symbol,
+                    Bank = s.Bank,
+                    Account = s.Account
+                }).ToList();
 
-            }).ToList();
-            AccountId = Transactions.FirstOrDefault().AccountId;
 
+            return new JsonResult(new { transa = listOfTransa });
         }
     }
 }
