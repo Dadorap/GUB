e301364c166f343d9326101dc46cac2ab6b506ad
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-04-11 21:03:36 +0200

    added a template to create account

--- ViewModels/ViewModel/Accounts/AccountViewModel.cs
+++ ViewModels/ViewModel/Accounts/AccountViewModel.cs
@@ -8,11 +8,8 @@ namespace ViewModels.ViewModel.Accounts
 {
     public class AccountViewModel
     {
-
         public string Frequency { get; set; } = null!;
-
         public DateOnly Created { get; set; }
-
         public decimal Balance { get; set; }
     }
 }
