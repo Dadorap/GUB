c6d80be37c2a598c21ef9f2b22e3080d47542f0d
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-04-21 13:12:12 +0200

    added set email confirmed to true by default so creating new user can login

--- GUB/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GUB/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -114,6 +114,10 @@ namespace ViewModels.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                // Set email confirmed to true
+                user.EmailConfirmed = true;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
