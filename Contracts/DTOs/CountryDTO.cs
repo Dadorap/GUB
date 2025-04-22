77ba7cbfc29d31f5a6d05666f89f7d122a80fcd5
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2025-04-22 19:21:06 +0200

    Removed Contracts folder was not needed

--- Contracts/DTOs/CountryDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Contracts.DTOs;
-
-public class CountryDTO
-{
-    public string Country { get; set; }
-    public int Customers { get; set; }
-    public decimal Balance { get; set; }
-    public int Accounts { get; set; }
-}
--- Contracts/DTOs/ZenQuotesDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Contracts.DTOs;
-
-public class ZenQuotesDTO
-{
-    public string Quote { get; set; }
-    public string Author { get; set; }
-}
